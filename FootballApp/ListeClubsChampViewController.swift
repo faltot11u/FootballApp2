213fae8bc2217232ffff0d24b92df9c79ec90a88
Loic Faltot <loic.faltot@gmail.com>
2021-03-05 14:56:49 +0100

    add class squad

---
+++ FootballApp/ListeClubsChampViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListeClubsChampViewController.swift
+//  FootballApp
+//
+//  Created by Loic Faltot on 05/03/2021.
+//
+
+
+import Alamofire
+import UIKit
+
+
+
+
+class ListeClubsChampViewController: UITableViewController {
+    
+    
+    var squads:[Squad] = []
+    
+    func listBeer(){
+        AF.request("https://api.punkapi.com/v2/beers").response { response in
+
+            let json = try! JSON(data: response.data!)
+
+            let jsonArray = json.arrayValue
+
+            for beer in jsonArray {
+                self.beers.append(Beer(Name: beer["name"].stringValue, Degree: beer["abv"].stringValue, Description: beer["description"].stringValue, PictureUrl: beer["image_url"].stringValue))
+
+            }
+            self.tableView.reloadData()
+
+        }
+    }
+}
---
+++ FootballApp/Squad.swift
@@ -0,0 +1,19 @@
+//
+//  Squad.swift
+//  FootballApp
+//
+//  Created by Loic Faltot on 05/03/2021.
+//
+
+import Foundation
+class Squad{
+    
+    var name: String
+    var picture_url: String
+    
+    init(name: String, picture_url: String) {
+        self.name = name
+        self.picture_url = picture_url
+    }
+    
+}
